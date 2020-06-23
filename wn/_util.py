07f9a9cadb2bfbbdc03cc0cb09b6a06d4640cf0f
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-06-23 11:50:27 +0800

    Add wn._util.progress_bar()
    It's not so complicated so we don't really need the tqdm or progress
    packages.

--- wn/_util.py
+++ wn/_util.py
@@ -1,4 +1,5 @@
 
+from typing import Sequence
 from pathlib import Path
 
 
@@ -6,3 +7,49 @@ def is_gzip(path: Path) -> bool:
     """Return True if the file at *path* appears to be gzipped."""
     with path.open('rb') as f:
         return f.read(2) == b'\x1F\x8B'
+
+
+def progress_bar(
+        message: str = '',
+        max: int = 0,
+        width: int = 40,
+        fmt: str = '\r{message}[{fill:<{width}}] ({current}/{max})',
+        fillchars: Sequence[str] = '#'):
+    """
+    Return a generator which yields a progres bar string.
+
+    Update the counter with the generator's send() method giving an
+    incremement value. Normally you'd want to print with end='' to
+    keep the progress bar on the same line.
+
+    Example:
+        >>> p = progress_bar('Progress: ', max=10, width=10)
+        >>> print(p.send(3))
+        Progress: [###       ] (3/10)
+    """
+
+    assert width >= 1, 'width must be 1 or greater'
+    fillnum = len(fillchars)
+    assert fillnum > 0, 'fillchars must be 1 or more characters'
+    step = max / width
+    fillchars = [''] + list(fillchars)
+
+    def fill(current: int) -> str:
+        if max > 0:
+            done = int((current / max) * width) * fillchars[-1]
+            part = fillchars[int(((current % step) / step) * len(fillchars))]
+            return done + part
+        else:
+            return '-' * width
+
+    def update():
+        data = {'message': message, 'fill': '', 'width': width,
+                'current': 0, 'max': max or '?'}
+        while True:
+            data['fill'] = fill(data['current'])
+            increment = yield fmt.format(**data)
+            data['current'] = min(data['current'] + increment, max)
+
+    generator = update()
+    next(generator)
+    return generator
