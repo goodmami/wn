4c5fc7e7ed832acb3f29ea21bd704f380db3150e
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-07-27 15:02:22 +0800

    Move wn.Error definition to wn/__init__.py
    This is just so the printed error message shows the error class with
    the public API name wn.Error instead of wn._exceptions.Error

--- wn/__init__.py
+++ wn/__init__.py
@@ -17,7 +17,16 @@ __all__ = (
     'Error',
 )
 
-from wn._exceptions import Error
+
+# This exception are defined here so the traceback says, e.g.,
+# `wn.Error` and not `wn._exceptions.Error`; if this behavior can be
+# cleanly implmented in some other way, then I'd be happy to move
+# non-import code out of __init__.py
+
+class Error(Exception):
+    """Generic error class for invalid wordnet operations."""
+
+
 from wn._meta import __version__
 from wn._projects import get_project_info
 from wn._store import add
--- wn/_exceptions.py
+++
@@ -1,7 +0,0 @@
-
-class Error(Exception):
-    """Generic error class for invalid wordnet operations."""
-
-
-class WordNetWarning(Warning):
-    """Issued on problems that could be fixed."""
