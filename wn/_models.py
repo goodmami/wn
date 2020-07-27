606adf7d0ff1fb7f6543fa30c4652bd622f18de7
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-07-27 15:16:37 +0800

    Abstrace Sense and Synset; add accessor methods

--- wn/_models.py
+++ wn/_models.py
@@ -1,5 +1,5 @@
 
-from typing import Optional, Union, List
+from typing import TypeVar, Optional, List, Iterator
 
 from wn import _store
 
@@ -24,9 +24,37 @@ class Word:
     def senses(self) -> List['Sense']:
         return _store.get_senses_for_entry(self.id)
 
+    def derived_words(self) -> List['Word']:
+        return [derived_sense.word()
+                for sense in self.senses()
+                for derived_sense in sense.derivations()]
 
-class Synset:
-    __slots__ = 'id', 'pos', 'ili'
+
+T = TypeVar('T', bound='_Relatable')
+
+
+class _Relatable:
+    __slots__ = 'id',
+
+    def __init__(self):
+        self.id: str = ''
+
+    def get_related(self: T, relation: str) -> List[T]:
+        raise NotImplementedError
+
+    def closure(self: T, relation: str) -> Iterator[T]:
+        visited = set()
+        queue = self.get_related(relation)
+        while queue:
+            relatable = queue.pop(0)
+            if relatable.id not in visited:
+                visited.add(relatable.id)
+                yield relatable
+                queue.extend(relatable.get_related(relation))
+
+
+class Synset(_Relatable):
+    __slots__ = 'pos', 'ili'
 
     def __init__(self, id: str, pos: str, ili: str = None):
         self.id = id
@@ -45,12 +73,50 @@ class Synset:
     def senses(self) -> List['Sense']:
         return _store.get_senses_for_synset(self.id)
 
-
-class Sense:
-    __slots__ = 'id', '_entry_id', '_synset_id', 'key'
     def get_related(self, *args: str) -> List['Synset']:
         return _store.get_synset_relations(self.id, args)
 
+    def holonyms(self) -> List['Synset']:
+        return self.get_related(
+            'holonym',
+            'holo_location',
+            'holo_member',
+            'holo_part',
+            'holo_portion',
+            'holo_substance',
+        )
+
+    def meronyms(self) -> List['Synset']:
+        return self.get_related(
+            'meronym',
+            'mero_location',
+            'mero_member',
+            'mero_part',
+            'mero_portion',
+            'mero_substance',
+        )
+
+    def hypernyms(self) -> List['Synset']:
+        return self.get_related(
+            'hypernym',
+            'instance_hypernym'
+        )
+
+    def hyponyms(self) -> List['Synset']:
+        return self.get_related(
+            'hyponym',
+            'instance_hyponym'
+        )
+
+    def antonyms(self) -> List['Synset']:
+        return self.get_related('antonym')
+
+    def similar(self) -> List['Synset']:
+        return self.get_related('similar')
+
+
+class Sense(_Relatable):
+    __slots__ = '_entry_id', '_synset_id', 'key'
 
     def __init__(self, id: str, entry_id: str, synset_id: str, key: str = None):
         self.id = id
@@ -70,3 +136,17 @@ class Sense:
     def get_related(self, *args: str) -> List['Sense']:
         return _store.get_sense_relations(self.id, args)
 
+    def get_related_synsets(self, *args: str) -> List['Synset']:
+        return _store.get_sense_synset_relations(self.id, args)
+
+    def derivations(self) -> List['Sense']:
+        return self.get_related('derivation')
+
+    def pertainyms(self) -> List['Sense']:
+        return self.get_related('pertainym')
+
+    def antonyms(self) -> List['Sense']:
+        return self.get_related('antonym')
+
+    def similar(self) -> List['Sense']:
+        return self.get_related('similar')
