70797442ce38ff59f8690506a7098a2a47ef261b
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-07-27 23:11:03 +0800

    Add starter wn.metrics module

---
+++ wn/metrics.py
@@ -0,0 +1,13 @@
+
+from wn._models import Word, Synset
+
+
+# Word-based Metrics
+
+def ambiguity(word: Word) -> int:
+    return len(word.synsets())
+
+
+def average_ambiguity(synset: Synset) -> float:
+    words = synset.words()
+    return sum(len(word.synsets()) for word in words) / len(words)
