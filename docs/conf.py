580fc71d94c3e81776bf2e2f803b542385e135f4
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-10-30 16:40:55 +0800

    Add intersphinx mappings (for type hints)

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,13 @@ autodoc_typehints = 'description'
 # autodoc_typehints = 'signature'
 # autodoc_typehints = 'none'
 
+# -- Options for intersphinx extension ---------------------------------------
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'requests': ('https://requests.readthedocs.io/en/master/', None),
+}
 
 # -- Options for sphinx_copybutton extension ---------------------------------
 
