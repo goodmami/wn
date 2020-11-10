cf703ae0614286c4e6e068d1f57d942fb0fba475
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-11-10 15:26:50 +0800

    Add similarity.wup(), rename similarity funcs
    I'm not sure wup() is giving the right scores yet. I still need to try
    with the PWN instead of the English WordNet 2020. But I'll say this
    resolves #2.
    
    And similarity.wup_similarity() seemed redundant, so now it's
    similarity.wup(), similarity.path(), etc.

--- wn/similarity.py
+++ wn/similarity.py
@@ -1,12 +1,21 @@
 
+from wn._core import Synset
 
-def path_similarity(synset1, synset2):
+
+def path(synset1, synset2):
+    """Return the Path similarity of *synset1* and *synset2*."""
     pass
 
 
-def wup_similarity(synset1, synset2):
-    pass
+def wup(synset1: Synset, synset2: Synset) -> float:
+    """Return the Wu-Palmer similarity of *synset1* and *synset2*."""
+    lch = synset1.lowest_common_hypernyms(synset2, simulate_root=True)[0]
+    n = lch.max_depth() + 1
+    n1 = len(synset1.shortest_path(lch, simulate_root=True))
+    n2 = len(synset2.shortest_path(lch, simulate_root=True))
+    return (2 * n) / (n1 + n2 + 2 * n)
 
 
-def lch_similarity(synset1, synset2):
+def lch(synset1: Synset, synset2: Synset) -> float:
+    """Return the Leacock-Chodorow similarity of *synset1* and *synset2*."""
     pass
