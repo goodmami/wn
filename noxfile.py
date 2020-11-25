02964dfe983aa23f8e47d766543036d0647d1bdf
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-11-25 20:47:14 +0800

    Simplify CI workflow

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,9 @@
 import nox
 
-PYTHON_VERSIONS = sorted(['3.6', '3.7', '3.8', '3.9'])
 
-@nox.session(python=PYTHON_VERSIONS)
+@nox.session
 def lint(session):
     session.install('flake8', 'mypy')
     session.install('.')
-    # session.run('python', '-m', 'flit', 'build')
     session.run('flake8', '--max-line-length', '88', 'wn')
     session.run('mypy', 'wn')
