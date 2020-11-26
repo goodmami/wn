0c30863d17ba00a8600b6e259d4b1277be950723
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-11-26 14:12:04 +0800

    Add 'test' session to noxfile and github workflows

--- noxfile.py
+++ noxfile.py
@@ -7,3 +7,10 @@ def lint(session):
     session.install('.')
     session.run('flake8', '--max-line-length', '88', 'wn')
     session.run('mypy', 'wn')
+
+
+@nox.session
+def test(session):
+    session.install('pytest')
+    session.install('.')
+    session.run('pytest')
