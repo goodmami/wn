1f45daed1120bd17e56986cd370d24e95e015c1d
Michael Wayne Goodman <goodman.m.w@gmail.com>
2021-01-19 12:21:16 +0800

    Fix test for Python 3.6

--- tests/db_test.py
+++ tests/db_test.py
@@ -8,6 +8,6 @@ import wn
 
 @pytest.mark.usefixtures('mini_db')
 def test_schema_compatibility():
-    conn = sqlite3.connect(wn.config.database_path)
+    conn = sqlite3.connect(str(wn.config.database_path))
     schema_hash = wn._db.schema_hash(conn)
     assert schema_hash in wn._db.COMPATIBLE_SCHEMA_HASHES
