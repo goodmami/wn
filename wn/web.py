4dc3bdc5f079c364a6179ae015432b5e20c5624f
Michael Wayne Goodman <goodman.m.w@gmail.com>
2021-05-28 10:45:17 +0800

    Follow JSON-API spec, add single lexicon endpoint

--- wn/web.py
+++ wn/web.py
@@ -1,5 +1,6 @@
 
 from functools import wraps
+from urllib.parse import urlsplit, parse_qs, urlencode
 
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
@@ -10,23 +11,30 @@ import wn
 DEFAULT_PAGINATION_LIMIT = 50
 
 
-def paginate(name, proto):
+def paginate(proto):
 
     def paginate_wrapper(func):
 
         @wraps(func)
         async def _paginate_wrapper(request):
-            query = request.query_params
-            offset = abs(int(query.get('offset', 0)))
-            limit = abs(int(query.get('limit', DEFAULT_PAGINATION_LIMIT)))
+            url = str(request.url)
+            query = dict(request.query_params)
+            offset = abs(int(query.pop('offset', 0)))
+            limit = abs(int(query.pop('limit', DEFAULT_PAGINATION_LIMIT)))
 
             obj = await func(request)
+            total = len(obj['data'])
 
-            seq = obj[name]
-            obj[name] = [proto(x, request) for x in seq[offset:offset+limit]]
-            obj['offset'] = offset
-            obj['limit'] = limit
-            obj['total'] = len(seq)
+            links = {
+                'first': replace_query_params(url, offset=0),
+                'last': replace_query_params(url, offset=(total//limit)*limit),
+                'prev': replace_query_params(url, offset=max(0, offset - limit)),
+                'next': replace_query_params(url, offset=offset + limit),
+            }
+            obj['data'] = [proto(x, request)
+                           for x in obj['data'][offset:offset+limit]]
+            obj.setdefault('meta', {}).update(total=total)
+            obj.setdefault('links', {}).update(links)
 
             return JSONResponse(obj)
 
@@ -35,80 +43,175 @@ def paginate(name, proto):
     return paginate_wrapper
 
 
+def replace_query_params(url: str, **params) -> str:
+    u = urlsplit(url)
+    q = parse_qs(u.query)
+    q.update(params)
+    qs = urlencode(q, doseq=True)
+    return u._replace(query=qs).geturl()
+
+
 # Data-making functions
 
+def _url_for_obj(request, name, obj, lexicon=None):
+    if lexicon is None:
+        lexicon = obj.lexicon().specifier()
+    kwargs = {
+        'lexicon': lexicon,
+        name: obj.id
+    }
+    return request.url_for(name, **kwargs)
+
+
 def make_lexicon(lex, request):
+    spec = lex.specifier()
     return {
         'id': lex.id,
-        'version': lex.version,
-        'label': lex.label,
-        'language': lex.language,
-        'license': lex.license,
-        'synsets': request.url_for('synsets', lexicon=lex.specifier()),
+        'type': 'lexicon',
+        'attributes': {
+            'version': lex.version,
+            'label': lex.label,
+            'language': lex.language,
+            'license': lex.license,
+        },
+        'links': {
+            'self': request.url_for('lexicon', lexicon=spec)
+        },
+        'relationships': {
+            'words': {
+                'links': {'related': request.url_for('words', lexicon=spec)},
+            },
+            'synsets': {
+                'links': {'related': request.url_for('synsets', lexicon=spec)},
+            },
+            'senses': {
+                'links': {'related': request.url_for('senses', lexicon=spec)},
+            },
+        }
     }
 
 
 def make_word(w, request):
+    lex_spec = w.lexicon().specifier()
+    senses = w.senses()
+    synsets = w.synsets()
     return {
         'id': w.id,
-        'pos': w.pos,
-        'lemma': w.lemma(),
-        'forms': w.forms(),
-        'senses': [
-            request.url_for('sense', lexicon=s.lexicon().specifier(), sense=s.id)
-            for s in w.senses()
-        ],
-        'lexicon': request.url_for(
-            'lexicons', lexicon=w.lexicon().specifier()
-        ),
+        'type': 'word',
+        'attributes': {
+            'pos': w.pos,
+            'lemma': w.lemma(),
+            'forms': w.forms(),
+        },
+        'links': {
+            'self': _url_for_obj(request, 'word', w, lexicon=lex_spec)
+        },
+        'relationships': {
+            'senses': {'data': [{'type': 'sense', 'id': s.id} for s in senses]},
+            'synsets': {'data': [{'type': 'synset', 'id': ss.id} for ss in synsets]},
+            'lexicon': {
+                'links': {'related': request.url_for('lexicon', lexicon=lex_spec)}
+            }
+        },
+        'included': [
+            {'type': 'sense',
+             'id': s.id,
+             'links': {'self': _url_for_obj(request, 'sense', s)}}
+            for s in senses
+        ] + [
+            {'type': 'synset',
+             'id': ss.id,
+             'links': {'self': _url_for_obj(request, 'synset', ss)}}
+            for ss in synsets
+        ]
     }
 
 
 def make_sense(s, request):
-    word = s.word()
-    synset = s.synset()
+    lex_spec = s.lexicon().specifier()
+    w = s.word()
+    ss = s.synset()
     return {
         'id': s.id,
-        'word': request.url_for(
-            'word', lexicon=word.lexicon().specifier(), word=word.id
-        ),
-        'synset': request.url_for(
-            'synset', lexicon=synset.lexicon().specifier(), synset=synset.id
-        ),
-        'lexicon': request.url_for(
-            'lexicons', lexicon=s.lexicon().specifier()
-        ),
+        'type': 'sense',
+        'links': {
+            'self': _url_for_obj(request, 'sense', s, lexicon=lex_spec)
+        },
+        'relationships': {
+            'word': {'data': {'type': 'word', 'id': w.id}},
+            'synset': {'data': {'type': 'synset', 'id': ss.id}},
+            'lexicon': {
+                'links': {'related': request.url_for('lexicon', lexicon=lex_spec)}
+            }
+        },
+        'included': [
+            {'type': 'word',
+             'id': w.id,
+             'attributes': {
+                'pos': w.pos,
+                'lemma': w.lemma(),
+                'forms': w.forms(),
+             },
+             'links': {'self': _url_for_obj(request, 'word', w)}},
+            {'type': 'synset',
+             'id': ss.id,
+             'links': {'self': _url_for_obj(request, 'synset', ss)}}
+        ]
     }
 
 
 def make_synset(ss, request):
+    lex_spec = ss.lexicon().specifier()
+    senses = ss.senses()
+    words = ss.words()
     return {
         'id': ss.id,
-        'pos': ss.pos,
-        'ili': ss._ili,
-        'members': [
-            request.url_for('sense', lexicon=s.lexicon().specifier(), sense=s.id)
-            for s in ss.senses()
-        ],
-        # 'relations': [],
-        'lexicon': request.url_for(
-            'lexicons', lexicon=ss.lexicon().specifier()
-        ),
+        'type': 'synset',
+        'attributes': {
+            'pos': ss.pos,
+            'ili': ss._ili,
+        },
+        'links': {
+            'self': _url_for_obj(request, 'synset', ss, lexicon=lex_spec)
+        },
+        'relationships': {
+            'members': {'data': [{'type': 'sense', 'id': s.id} for s in senses]},
+            'words': {'data': [{'type': 'word', 'id': w.id} for w in words]},
+            'lexicon': {
+                'links': {'related': request.url_for('lexicon', lexicon=lex_spec)}
+            }
+        },
+        'included': [
+            {'type': 'sense',
+             'id': s.id,
+             'links': {'self': _url_for_obj(request, 'sense', s)}}
+            for s in senses
+        ] + [
+            {'type': 'word',
+             'id': w.id,
+             'links': {'self': _url_for_obj(request, 'word', w)}}
+            for w in words
+        ]
     }
 
 
-@paginate('lexicons', make_lexicon)
+@paginate(make_lexicon)
 async def lexicons(request):
     query = request.query_params
-    lexicon = request.path_params.get('lexicon') or query.get('lexicon')
     _lexicons = wn.lexicons(
-        lexicon=lexicon,
+        lexicon=query.get('lexicon'),
         lang=query.get('lang'),
     )
-    return {'lexicons': _lexicons}
+    return {'data': _lexicons}
 
 
-@paginate('words', make_word)
+async def lexicon(request):
+    path_params = request.path_params
+    lex = wn.lexicons(lexicon=path_params['lexicon'])[0]
+    return JSONResponse({'data': make_lexicon(lex, request)})
+
+
+@paginate(make_word)
 async def words(request):
     query = request.query_params
     lexicon = request.path_params.get('lexicon') or query.get('lexicon')
@@ -118,7 +221,7 @@ async def words(request):
         lexicon=lexicon,
         lang=query.get('lang'),
     )
-    return {'words': _words}
+    return {'data': _words}
 
 
 async def word(request):
@@ -127,7 +230,7 @@ async def word(request):
     return JSONResponse({'word': make_word(word, request)})
 
 
-@paginate('senses', make_sense)
+@paginate(make_sense)
 async def senses(request):
     query = request.query_params
     lexicon = request.path_params.get('lexicon') or query.get('lexicon')
@@ -137,7 +240,7 @@ async def senses(request):
         lexicon=lexicon,
         lang=query.get('lang'),
     )
-    return {'senses': _senses}
+    return {'data': _senses}
 
 
 async def sense(request):
@@ -146,7 +249,7 @@ async def sense(request):
     return JSONResponse({'sense': make_sense(sense, request)})
 
 
-@paginate('synsets', make_synset)
+@paginate(make_synset)
 async def synsets(request):
     query = request.query_params
     lexicon = request.path_params.get('lexicon') or query.get('lexicon')
@@ -157,7 +260,7 @@ async def synsets(request):
         lexicon=lexicon,
         lang=query.get('lang'),
     )
-    return {'synsets': _synsets}
+    return {'data': _synsets}
 
 
 async def synset(request):
@@ -168,7 +271,7 @@ async def synset(request):
 
 routes = [
     Route('/lexicons', endpoint=lexicons),
-    Route('/lexicons/{lexicon}', endpoint=lexicons),
+    Route('/lexicons/{lexicon}', endpoint=lexicon),
     Route('/lexicons/{lexicon}/words', endpoint=words),
     Route('/lexicons/{lexicon}/words/{word}', endpoint=word),
     Route('/lexicons/{lexicon}/senses', endpoint=senses),
