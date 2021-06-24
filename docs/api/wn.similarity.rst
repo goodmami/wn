545f7e229dcd9b41120c4a0235f11465c4e9cb0d
Michael Wayne Goodman <goodman.m.w@gmail.com>
2021-06-24 12:54:01 +0800

    Improve documentation for similarity metrics

--- docs/api/wn.similarity.rst
+++ docs/api/wn.similarity.rst
@@ -3,6 +3,83 @@ wn.similarity
 
 .. automodule:: wn.similarity
 
+Taxonomy-based Metrics
+----------------------
+
+The `Path <Path Similarity_>`_, `Leacock-Chodorow <Leacock-Chodorow
+Similarity_>`_, and `Wu-Palmer <Wu-Palmer Similarity_>`_ similarity
+metrics work by finding path distances in the hypernym/hyponym
+taxonomy. As such, they are most useful when the synsets are, in fact,
+arranged in a taxonomy. For the Princeton WordNet and derivative
+wordnets, synsets for nouns and verbs are arranged taxonomically: the
+nouns mostly form a single structure with a single root while verbs
+form many smaller structures with many roots. Synsets for the other
+parts of speech do not use hypernym/hyponym relations at all. This
+situation may be different for other wordnet projects or future
+versions of the English wordnets.
+
+The similarity metrics tend to fail when the synsets are not connected
+by some path. When the synsets are in different parts of speech, or
+even in separate lexicons, this failure is acceptable and
+expected. But for cases like the verbs in the Princeton WordNet, it
+might be more useful to pretend that there is some unique root for all
+verbs so as to create a path connecting any two of them. For this
+purpose, the *simulate_root* parameter is available on the
+:func:`path`, :func:`lch`, and :func:`wup` functions, where it is
+passed on to calls to :meth:`wn.Synset.shortest_path` and
+:meth:`wn.Synset.lowest_common_hypernyms`. Setting *simulate_root* to
+:python:`True` can, however, give surprising results if the words are
+from a different lexicon. Currently, computing similarity for synsets
+from a different part of speech raises an error.
+
+
+Path Similarity
+'''''''''''''''
+
+When :math:`p` is the length of the shortest path between two synsets,
+the path similarity is:
+
+.. math::
+
+   \frac{1}{p + 1}
+
+The similarity score ranges between 0.0 and 1.0, where the higher the
+score is, the more similar the synsets are. The score is 1.0 when a
+synset is compared to itself, and 0.0 when there is no path between
+the two synsets (i.e., the path distance is infinite).
+
 .. autofunction:: path
-.. autofunction:: wup
+
+
+.. _leacock-chodorow-similarity:
+
+Leacock-Chodorow Similarity
+'''''''''''''''''''''''''''
+
+When :math:`p` is the length of the shortest path between two synsets
+and :math:`d` is the maximum taxonomy depth, the Leacock-Chodorow
+similarity is:
+
+.. math::
+
+   -\text{log}(\frac{p + 1}{2d})
+
 .. autofunction:: lch
+
+
+Wu-Palmer Similarity
+''''''''''''''''''''
+
+When *LCS* is the lowest common hypernym (also called "least common
+subsumer") between two synsets, :math:`i` is the shortest path
+distance from the first synset to *LCS*, :math:`j` is the shortest
+path distance from the second synset to *LCS*, and :math:`k` is the
+number of nodes (distance + 1) from *LCS* to the root node, then the
+Wu-Palmer similarity is:
+
+.. math::
+
+   \frac{2k}{i + j + 2k}
+
+.. autofunction:: wup
+
--- wn/similarity.py
+++ wn/similarity.py
@@ -1,4 +1,6 @@
 
+"""Synset similarity metrics."""
+
 import math
 
 import wn
@@ -8,23 +10,61 @@ from wn._core import Synset
 def path(synset1, synset2):
     """Return the Path similarity of *synset1* and *synset2*.
 
-    When :math:`d` is the length of the shortest path from *synset1*
-    to *synset2*, the path similarity is: :math:`\\frac{1}{d + 1}`
+    Arguments:
+        synset1: The first synset to compare.
+        synset2: The second synset to compare.
+        simulate_root: When :python:`True`, a fake root node connects
+            all other roots; default: :python:`False`.
 
-    """
-    distance = len(synset1.shortest_path(synset2, simulate_root=True))
+    Example:
+        >>> import wn
+        >>> from wn.similarity import path
+        >>> ewn = wn.Wordnet('ewn:2020')
+        >>> spatula = ewn.synsets('spatula')[0]
+        >>> path(spatula, ewn.synsets('pancake')[0])
+        0.058823529411764705
+        >>> path(spatula, ewn.synsets('utensil')[0])
+        0.2
+        >>> path(spatula, spatula)
+        1.0
+        >>> flip = ewn.synsets('flip', pos='v')[0]
+        >>> turn_over = ewn.synsets('turn over', pos='v')[0]
+        >>> path(flip, turn_over)
+        0.0
+        >>> path(flip, turn_over, simulate_root=True)
+        0.16666666666666666
+
+     """
     return 1 / (distance + 1)
 
 
 def wup(synset1: Synset, synset2: Synset) -> float:
     """Return the Wu-Palmer similarity of *synset1* and *synset2*.
 
-    When *lch* is the lowest common hypernym for *synset1* and
-    *synset2*, *n1* is the shortest path distance from *synset1* to
-    *lch*, *n2* is the shortest path distance from *synset2* to *lch*,
-    and *n3* is the number of nodes (distance + 1) from *lch* to the
-    root node, then the Wu-Palmer similarity is:
-    :math:`\\frac{2(n3)}{n1 + n2 + 2(n3)}`
+    Arguments:
+        synset1: The first synset to compare.
+        synset2: The second synset to compare.
+        simulate_root: When :python:`True`, a fake root node connects
+            all other roots; default: :python:`False`.
+
+    Raises:
+        wn.Error: When no path connects the *synset1* and *synset2*.
+
+    Example:
+        >>> import wn
+        >>> from wn.similarity import wup
+        >>> ewn = wn.Wordnet('ewn:2020')
+        >>> spatula = ewn.synsets('spatula')[0]
+        >>> wup(spatula, ewn.synsets('pancake')[0])
+        0.2
+        >>> wup(spatula, ewn.synsets('utensil')[0])
+        0.8
+        >>> wup(spatula, spatula)
+        1.0
+        >>> flip = ewn.synsets('flip', pos='v')[0]
+        >>> turn_over = ewn.synsets('turn over', pos='v')[0]
+        >>> wup(flip, turn_over, simulate_root=True)
+        0.2857142857142857
 
     """
     lch = synset1.lowest_common_hypernyms(synset2, simulate_root=True)[0]
@@ -32,11 +72,40 @@ def wup(synset1: Synset, synset2: Synset) -> float:
     n1 = len(synset1.shortest_path(lch, simulate_root=True))
     n2 = len(synset2.shortest_path(lch, simulate_root=True))
     return (2 * n3) / (n1 + n2 + 2 * n3)
+def lch(
+    synset1: Synset,
+    synset2: Synset,
+    max_depth: int,
+    simulate_root: bool = False
+) -> float:
+    """Return the Leacock-Chodorow similarity between *synset1* and *synset2*.
 
+    Arguments:
+        synset1: The first synset to compare.
+        synset2: The second synset to compare.
+        max_depth: The taxonomy depth (see :func:`wn.taxonomy.taxonomy_depth`)
+        simulate_root: When :python:`True`, a fake root node connects
+            all other roots; default: :python:`False`.
 
-def lch(synset1: Synset, synset2: Synset, max_depth: int = 0) -> float:
-    """Return the Leacock-Chodorow similarity of *synset1* and *synset2*."""
-    distance = len(synset1.shortest_path(synset2, simulate_root=True))
+    Example:
+        >>> import wn, wn.taxonomy
+        >>> from wn.similarity import lch
+        >>> ewn = wn.Wordnet('ewn:2020')
+        >>> n_depth = wn.taxonomy.taxonomy_depth(ewn, 'n')
+        >>> spatula = ewn.synsets('spatula')[0]
+        >>> lch(spatula, ewn.synsets('pancake')[0], n_depth)
+        0.8043728156701697
+        >>> lch(spatula, ewn.synsets('utensil')[0], n_depth)
+        2.0281482472922856
+        >>> lch(spatula, spatula, n_depth)
+        3.6375861597263857
+        >>> v_depth = taxonomy.taxonomy_depth(ewn, 'v')
+        >>> flip = ewn.synsets('flip', pos='v')[0]
+        >>> turn_over = ewn.synsets('turn over', pos='v')[0]
+        >>> lch(flip, turn_over, v_depth, simulate_root=True)
+        1.3862943611198906
+
+    """
     if max_depth <= 0:
         raise wn.Error('max_depth must be greater than 0')
     return -math.log((distance + 1) / (2 * max_depth))
