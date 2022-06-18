ea450b2353aea8249998bcb376eef308ea151f0a
Michael Wayne Goodman <goodman.m.w@gmail.com>
2022-06-17 23:18:01 -0700

    Add web extra to noxfile for linting

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import nox
 
 @nox.session
 def lint(session):
-    session.install('.[test]')
+    session.install('.[test,web]')
     session.run('flake8', '--max-line-length', '88', 'wn', 'tests')
     session.run('mypy', '--python-version', '3.7', 'wn')
 
