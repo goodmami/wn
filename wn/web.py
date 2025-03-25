4d266135b3e71414b878e72f884bc6078dc50768
Michael Wayne Goodman <goodman.m.w@gmail.com>
2025-03-24 20:07:54 -0700

    Make web implementation a bit tighter, add tests
    Unfortunately the tests don't work at the moment due to a monkeypatching
    issue, but aside from that they pass.

--- wn/web.py
+++ wn/web.py
@@ -60,6 +60,17 @@ def replace_query_params(url: str, **params) -> str:
     return u._replace(query=qs).geturl()
 
 
+# Wordnet-instantiation
+
+def _init_wordnet(
+    lexicon: str = '*',
+    lang: Optional[str] = None,
+) -> wn.Wordnet:
+    if lexicon == '*' and lang is not None:
+        lexicon = ' '.join(lex.specifier() for lex in wn.lexicons(lang=lang))
+    return wn.Wordnet(lexicon)
+
+
 # Data-making functions
 
 def _url_for_obj(
@@ -197,6 +208,8 @@ def make_synset(ss: wn.Synset, request: Request, basic: bool = False) -> dict:
     return d
 
 
+# Route handlers
+
 @paginate(make_lexicon)
 async def lexicons(request):
     query = request.query_params
@@ -213,67 +226,97 @@ async def lexicon(request):
     return JSONResponse({'data': make_lexicon(lex, request)})
 
 
-@paginate(make_word)
-async def words(request):
+def _get_words(wordnet: wn.Wordnet, request: Request) -> dict:
     query = request.query_params
-    lexicon = request.path_params.get('lexicon') or query.get('lexicon')
-    _words = wn.words(
+    _words = wordnet.words(
         form=query.get('form'),
         pos=query.get('pos'),
-        lexicon=lexicon,
-        lang=query.get('lang'),
     )
     return {'data': _words}
 
 
+@paginate(make_word)
+async def all_words(request):
+    query = request.query_params
+    wordnet = _init_wordnet(lexicon=query.get('lexicon'), lang=query.get('lang'))
+    return _get_words(wordnet, request)
+
+
+@paginate(make_word)
+async def words(request):
+    wordnet = _init_wordnet(request.path_params['lexicon'])
+    return _get_words(wordnet, request)
+
+
 async def word(request):
     path_params = request.path_params
-    word = wn.word(path_params['word'], lexicon=path_params['lexicon'])
+    wordnet = _init_wordnet(request.path_params['lexicon'])
+    word = wordnet.word(path_params['word'])
     return JSONResponse({'data': make_word(word, request)})
 
 
-@paginate(make_sense)
-async def senses(request):
+def _get_senses(wordnet: wn.Wordnet, request: Request) -> dict:
     query = request.query_params
     path = request.path_params
-    lexicon = path.get('lexicon') or query.get('lexicon')
     if 'word' in path:
-        _senses = wn.word(path['word'], lexicon=lexicon).senses()
+        _senses = wordnet.word(path['word']).senses()
     elif 'synset' in path:
-        _senses = wn.synset(path['synset'], lexicon=lexicon).members()
+        _senses = wordnet.synset(path['synset']).senses()
     else:
-        _senses = wn.senses(
+        _senses = wordnet.senses(
             form=query.get('form'),
             pos=query.get('pos'),
-            lexicon=lexicon,
-            lang=query.get('lang'),
         )
     return {'data': _senses}
 
 
+@paginate(make_sense)
+async def all_senses(request):
+    query = request.query_params
+    wordnet = _init_wordnet(lexicon=query.get('lexicon'), lang=query.get('lang'))
+    return _get_senses(wordnet, request)
+
+
+@paginate(make_sense)
+async def senses(request):
+    wordnet = _init_wordnet(request.path_params['lexicon'])
+    return _get_senses(wordnet, request)
+
+
 async def sense(request):
     path_params = request.path_params
-    sense = wn.sense(path_params['sense'], lexicon=path_params['lexicon'])
+    wordnet = _init_wordnet(path_params['lexicon'])
+    sense = wordnet.sense(path_params['sense'])
     return JSONResponse({'data': make_sense(sense, request)})
 
 
-@paginate(make_synset)
-async def synsets(request):
+def _get_synsets(wordnet: wn.Wordnet, request: Request) -> dict:
     query = request.query_params
-    lexicon = request.path_params.get('lexicon') or query.get('lexicon')
-    _synsets = wn.synsets(
+    _synsets = wordnet.synsets(
         form=query.get('form'),
         pos=query.get('pos'),
         ili=query.get('ili'),
-        lexicon=lexicon,
-        lang=query.get('lang'),
     )
     return {'data': _synsets}
 
 
+@paginate(make_synset)
+async def all_synsets(request):
+    query = request.query_params
+    wordnet = _init_wordnet(lexicon=query.get('lexicon'), lang=query.get('lang'))
+    return _get_synsets(wordnet, request)
+
+
+@paginate(make_synset)
+async def synsets(request):
+    wordnet = _init_wordnet(request.path_params['lexicon'])
+    return _get_synsets(wordnet, request)
+
+
 async def synset(request):
     path_params = request.path_params
-    synset = wn.synset(path_params['synset'], lexicon=path_params['lexicon'])
+    wordnet = _init_wordnet(path_params['lexicon'])
+    synset = wordnet.synset(path_params['synset'])
     return JSONResponse({'data': make_synset(synset, request)})
 
 
@@ -288,9 +331,9 @@ routes = [
     Route('/lexicons/{lexicon}/synsets', endpoint=synsets),
     Route('/lexicons/{lexicon}/synsets/{synset}', endpoint=synset),
     Route('/lexicons/{lexicon}/synsets/{synset}/members', endpoint=senses),
-    Route('/words', endpoint=words),
-    Route('/senses', endpoint=senses),
-    Route('/synsets', endpoint=synsets),
+    Route('/words', endpoint=all_words),
+    Route('/senses', endpoint=all_senses),
+    Route('/synsets', endpoint=all_synsets),
 ]
 
 app = Starlette(debug=True, routes=routes)
