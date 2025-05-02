ddbb25529f4c1907e887e141c6e59a59c2d19414
Michael Wayne Goodman <goodman.m.w@gmail.com>
2025-05-02 11:54:49 -0700

    Add more documentation for sensekey module
    And fix a mypy-detected bug.

--- wn/compat/sensekey.py
+++ wn/compat/sensekey.py
@@ -1,4 +1,46 @@
-"""Functions Related to Sense Keys"""
+"""Functions Related to Sense Keys
+
+Sense keys are identifiers of senses that (mostly) persist across
+wordnet versions. They are only used by the English wordnets. For the
+OMW lexicons derived from the Princeton WordNet and the EWN 2019/2020
+lexicons, the sense key is encoded in the ``identifier`` metadata of a
+Sense:
+
+>>> import wn
+>>> en = wn.Wordnet("omw-en:1.4")
+>>> sense = en.sense("omw-en-carrousel-02966372-n")
+>>> sense.metadata()
+{'identifier': 'carrousel%1:06:01::'}
+
+For OEWN 2021+ lexicons, the sense key is encoded in the sense ID, but
+some characters are escaped or replaced to ensure it is a valid XML
+ID.
+
+>>> oewn = wn.Wordnet("oewn:2024")
+>>> sense = oewn.sense("oewn-carousel__1.06.01..")
+>>> sense.id
+'oewn-carousel__1.06.01..'
+
+This module has two functions:
+
+1. :func:`sense_key_getter` creates a function for retrieving the
+   sense key for a given :class:`wn.Sense` object. Depending on the
+   lexicon, it will retrieve the sense key from metadata or it will
+   unescape the sense ID.
+
+2. :func:`sense_getter` creates a function for retrieving a
+   :class:`wn.Sense` object given a sense key. Depending on the
+   lexicon, it will build and use a mapping of sense key metadata to
+   :class:`wn.Sense` objects, or it will escape the sense key and use
+   the escaped form as the ``id`` argument for
+   :meth:`wn.Wordnet.sense`.
+
+.. seealso::
+
+   The documentation from the Princeton WordNet:
+   https://wordnet.princeton.edu/documentation/senseidx5wn
+
+"""
 
 from typing import Callable, Optional
 
@@ -69,10 +111,9 @@ def sense_key_getter(lexicon: str) -> SensekeyGetter:
     """Return a function that gets sense keys from senses.
 
     The *lexicon* argument determines how the function will retrieve
-    the sense key. For OMW and EWN 2019/2020 lexicons, the sense key
-    is in the ``identifier`` metadata. For OEWN 2021+ lexicons, the
-    sense ID is an escaped form of the sense key. For any unsupported
-    lexicon, an error is raised.
+    the sense key; i.e., whether it is from the ``identifier``
+    metadata or unescaping the sense ID. For any unsupported lexicon,
+    an error is raised.
 
     The function that is returned accepts one argument, a
     :class:`wn.Sense` (ideally from the same lexicon specified in the
@@ -113,12 +154,10 @@ def sense_getter(lexicon: str, wordnet: Optional[wn.Wordnet] = None) -> SenseGet
     """Return a function that gets the sense for a sense key.
 
     The *lexicon* argument determines how the function will retrieve
-    the sense. For OMW and EWN 2019/2020 lexicons, the sense key is in
-    the ``identifier`` metadata and a mapping of sense keys to senses
-    will be created internally. For OEWN 2021+ lexicons the sense ID
-    is an escaped form of the sense key, so the given sense key is
-    escaped, then the sense is retrieved by :meth:`wn.Wordnet.sense`.
-    For any unsupported lexicon, an error is raised.
+    the sense; i.e., whether a mapping between a sense's
+    ``identifier`` metadata and the sense will be created and used or
+    the escaped sense key is used as the sense ID. For any unsupported
+    lexicon, an error is raised.
 
     The optional *wordnet* object is used as the source of the
     returned :class:`wn.Sense` objects. If none is provided, a new
@@ -135,18 +174,27 @@ def sense_getter(lexicon: str, wordnet: Optional[wn.Wordnet] = None) -> SenseGet
     >>> get_sense("alabaster%3:01:00::")
     Sense('oewn-alabaster__3.01.00..')
 
+    .. warning::
+
+       The mapping built for the ``omw-en*`` or ``ewn`` lexicons
+       requires significant memory---around 100MiB---to use. The
+       ``oewn`` lexicons do not require such a mapping and the memory
+       usage is negligible.
+
     """
     if wordnet is None:
         wordnet = wn.Wordnet(lexicon)
 
     if lexicon in METADATA_LEXICONS:
         get_sense_key = sense_key_getter(lexicon)
-        sense_key_map = {get_sense_key(s): s for s in wordnet.senses()}
+        sense_key_map = {get_sense_key(s): s.id for s in wordnet.senses()}
         if None in sense_key_map:
             sense_key_map.pop(None)  # senses without sense keys
 
         def getter(sense_key: str) -> Optional[wn.Sense]:
-            return sense_key_map.get(sense_key)
+            if sense_id := sense_key_map.get(sense_key):
+                return wordnet.sense(sense_id)
+            return None
 
     elif lexicon in SENSE_ID_LEXICONS:
         lexid, _ = split_lexicon_specifier(lexicon)
