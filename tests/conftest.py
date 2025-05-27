3453761685e2a08eb45716f8a5c10df436c3b441
Michael Wayne Goodman <goodman.m.w@gmail.com>
2025-05-26 18:43:29 -0700

    Don't delete decompressed .xml files too early

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 
-from pathlib import Path
+import lzma
 import tempfile
+from pathlib import Path
 
 import pytest
 
@@ -41,6 +42,19 @@ def mini_db_dir(datadir):
         yield Path(dir)
 
 
+@pytest.fixture
+def mini_lmf_compressed(datadir):
+    data = (datadir / 'mini-lmf-1.0.xml').read_bytes()
+    with tempfile.NamedTemporaryFile(suffix='.xml.xz', delete=False) as file:
+        path = Path(file.name)
+        with lzma.open(path, "w") as f:
+            f.write(data)
+        try:
+            yield Path(file.name)
+        finally:
+            Path(file.name).unlink()
+
+
 @pytest.fixture(scope='session')
 def mini_db_1_1_dir(datadir):
     with tempfile.TemporaryDirectory('wn_data_mini_1_1') as dir:
@@ -48,6 +62,17 @@ def mini_db_1_1_dir(datadir):
             m.setattr(wn.config, 'data_directory', dir)
             wn.add(datadir / 'mini-lmf-1.0.xml')
             wn.add(datadir / 'mini-lmf-1.1.xml')
+            wn._db.clear_connections()
+
+        yield Path(dir)
+
+
+@pytest.fixture(scope='session')
+def mini_db_1_4_dir(datadir):
+    with tempfile.TemporaryDirectory('wn_data_mini_1_4') as dir:
+        with pytest.MonkeyPatch.context() as m:
+            m.setattr(wn.config, 'data_directory', dir)
+            wn.add(datadir / 'mini-lmf-1.4.xml')
             wn._db.clear_connections()
 
         yield Path(dir)
@@ -65,5 +90,13 @@ def mini_db(monkeypatch, mini_db_dir):
 def mini_db_1_1(monkeypatch, mini_db_1_1_dir):
     with monkeypatch.context() as m:
         m.setattr(wn.config, 'data_directory', mini_db_1_1_dir)
+        yield
+        wn._db.clear_connections()
+
+
+@pytest.fixture
+def mini_db_1_4(monkeypatch, mini_db_1_4_dir):
+    with monkeypatch.context() as m:
+        m.setattr(wn.config, 'data_directory', mini_db_1_4_dir)
         yield
         wn._db.clear_connections()
--- tests/project_test.py
+++ tests/project_test.py
@@ -13,12 +13,14 @@ def test_is_collection_directory(datadir):
 
 def test_get_project(datadir):
     proj = project.get_project(path=datadir / "test-package")
+    assert proj.type == "wordnet"
     assert proj.resource_file() == datadir / "test-package" / "test-wn.xml"
     assert proj.readme() == datadir / "test-package" / "README.md"
     assert proj.license() == datadir / "test-package" / "LICENSE"
     assert proj.citation() == datadir / "test-package" / "citation.bib"
 
     proj = project.get_project(path=datadir / "mini-lmf-1.0.xml")
+    assert proj.type == "wordnet"
     assert proj.resource_file() == datadir / "mini-lmf-1.0.xml"
     assert proj.readme() is None
     assert proj.license() is None
@@ -41,3 +43,13 @@ def test_iterpackages(datadir):
     }
     assert "mini-lmf-1.0.xml" in pkg_names
     assert "test-wn.xml" not in pkg_names
+
+
+def test_compressed_iterpackages(mini_lmf_compressed):
+    for pkg in project.iterpackages(mini_lmf_compressed):
+        assert pkg.type == "wordnet"
+        assert pkg.resource_file().exists()
+    # ensure cleanup of temporary data
+    assert not pkg.resource_file().exists()
+    # ensure original file not deleted
+    assert mini_lmf_compressed.exists()
