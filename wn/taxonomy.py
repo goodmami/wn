5acf7dc65660aa32c5880c811612d70474986a69
Francis Bond <bond@ieee.org>
2025-12-02 07:56:21 +0530

    Fixed doc string for taxonomy depth (#291)

--- wn/taxonomy.py
+++ wn/taxonomy.py
@@ -59,7 +59,7 @@ def leaves(wordnet: 'Wordnet', pos: Optional[str] = None) -> list['Synset']:
 
 
 def taxonomy_depth(wordnet: 'Wordnet', pos: str) -> int:
-    """Return the list of leaf synsets in *wordnet*.
+    """Return the maximum depth of the taxonomy for the given part of speech.
 
     Arguments:
 
